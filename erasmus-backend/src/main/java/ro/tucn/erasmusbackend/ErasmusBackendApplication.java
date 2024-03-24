320e13bb64e0ec5f8e24093054251cc2505b9eba
Vlad-Alexandru Bartolomei <vladalexandru.bartolomei@stud.h-da.de>
2024-03-24 17:50:20 +0200

    Project' starting point created

---
+++ erasmus-backend/src/main/java/ro/tucn/erasmusbackend/ErasmusBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.tucn.erasmusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasmusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasmusBackendApplication.class, args);
+	}
+
+}
---
+++ erasmus-backend/src/test/java/ro/tucn/erasmusbackend/ErasmusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tucn.erasmusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasmusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
